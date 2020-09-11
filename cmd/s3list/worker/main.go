ba749c9d1574b1e2471f2a6f45423595a595984b
Maxim Fateev <mfateev@gmail.com>
2020-09-10 21:31:57 -0700

    Renamed helloworld to s3list

---
+++ cmd/s3list/worker/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	s32 "github.com/aws/aws-sdk-go/service/s3"
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+	"log"
+	"temporal.io/aws-sdk/cmd/s3list"
+)
+
+const taskQueue = "s3list"
+
+func main() {
+	c, err := client.NewClient(client.Options{})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, taskQueue, worker.Options{})
+
+	w.RegisterWorkflow(s3list.Workflow)
+
+	err = w.Start()
+	if err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+	defer w.Stop()
+
+	options := client.StartWorkflowOptions{
+		TaskQueue: taskQueue,
+	}
+	wRun, err := c.ExecuteWorkflow(context.Background(), options, s3list.Workflow)
+	if err != nil {
+		log.Fatalln("Failure starting workflow", err)
+	}
+	var result s32.ListBucketsOutput
+	wRun.Get(context.Background(), &result)
+	for _, b := range result.Buckets {
+		fmt.Printf("* %s created on %s\n",
+			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	}
+	<-worker.InterruptCh()
+}
---
+++ cmd/s3list/workflow.go
@@ -0,0 +1,17 @@
+package s3list
+
+import (
+	s32 "github.com/aws/aws-sdk-go/service/s3"
+	"go.temporal.io/sdk/workflow"
+	"temporal.io/aws-sdk/awsclients"
+	"time"
+)
+
+func Workflow(ctx workflow.Context) (*s32.ListBucketsOutput, error) {
+	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:           "aws-sdk",
+		StartToCloseTimeout: 10 * time.Second,
+	})
+	s3 := awsclients.NewS3Stub()
+	return s3.ListBuckets(ctx, &s32.ListBucketsInput{})
+}
