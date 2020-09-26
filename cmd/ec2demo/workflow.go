93a67efcaa840af17959c8bf9afc18e8280f3b85
Maxim Fateev <mfateev@gmail.com>
2020-09-26 10:11:32 -0700

    Fixed activity timeouts

--- cmd/ec2demo/workflow.go
+++ cmd/ec2demo/workflow.go
@@ -3,6 +3,7 @@ package ec2demo
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 	"temporal.io/aws-sdk/awsclients"
 	"time"
@@ -10,18 +11,22 @@ import (
 
 func KeepInstance(ctx workflow.Context) error {
 	logger := workflow.GetLogger(ctx)
-	ctx = workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
-		TaskQueue:           "aws-sdk",
-		StartToCloseTimeout: 10 * time.Second,
-	})
 	ec2Client := awsclients.NewEC2Stub()
-	for {
+	for i := 0; i < 1000; i++ {
 		instanceId, err2 := launchInstance(ctx, ec2Client)
 		if err2 != nil {
 			return err2
 		}
+		ctx1 := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+			TaskQueue:           "aws-sdk",
+			StartToCloseTimeout: 24 * 364 * time.Hour,
+			HeartbeatTimeout:    time.Minute,
+			RetryPolicy: &temporal.RetryPolicy{
+				NonRetryableErrorTypes: []string{"foo"},
 
-		err := ec2Client.WaitUntilInstanceTerminated(ctx, &ec2.DescribeInstancesInput{
+			},
+		})
+		err := ec2Client.WaitUntilInstanceTerminated(ctx1, &ec2.DescribeInstancesInput{
 			InstanceIds: []*string{instanceId},
 		})
 		if err != nil {
@@ -30,12 +35,16 @@ func KeepInstance(ctx workflow.Context) error {
 
 		logger.Info("EC2 instance terminated.", "instanceId", *instanceId)
 	}
-	return nil
+	return workflow.NewContinueAsNewError(ctx, KeepInstance)
 }
 
 func launchInstance(ctx workflow.Context, ec2Client awsclients.EC2Client) (*string, error) {
 	logger := workflow.GetLogger(ctx)
-	reservation, err := ec2Client.RunInstances(ctx, &ec2.RunInstancesInput{
+	ctx1 := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:           "aws-sdk",
+		StartToCloseTimeout: 10 * time.Second,
+	})
+	reservation, err := ec2Client.RunInstances(ctx1, &ec2.RunInstancesInput{
 		ImageId:      aws.String("ami-0947d2ba12ee1ff75"),
 		InstanceType: aws.String("t2.nano"),
 		MinCount:     aws.Int64(1),
@@ -47,7 +56,12 @@ func launchInstance(ctx workflow.Context, ec2Client awsclients.EC2Client) (*stri
 	instanceId := reservation.Instances[0].InstanceId
 	logger.Info("Starting EC2 instance", "instanceId", *instanceId)
 
-	err = ec2Client.WaitUntilInstanceRunning(ctx, &ec2.DescribeInstancesInput{
+	ctx2 := workflow.WithActivityOptions(ctx, workflow.ActivityOptions{
+		TaskQueue:           "aws-sdk",
+		StartToCloseTimeout: 10 * time.Minute,
+		HeartbeatTimeout:    time.Minute,
+	})
+	err = ec2Client.WaitUntilInstanceRunning(ctx2, &ec2.DescribeInstancesInput{
 		InstanceIds: []*string{instanceId},
 	})
 	if err != nil {
