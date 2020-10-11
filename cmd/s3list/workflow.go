4e54fbc148068c983cb09c3199e3837f0926f22f
Maxim Fateev <mfateev@gmail.com>
2020-10-11 12:56:11 -0700

    Refactored client interfaces into own packages

--- cmd/s3list/workflow.go
+++ cmd/s3list/workflow.go
@@ -2,7 +2,7 @@ package s3list
 
 import (
 	s32 "github.com/aws/aws-sdk-go/service/s3"
-	"go.temporal.io/aws-sdk/awsclients"
+	"go.temporal.io/aws-sdk/awsclients/s3stub"
 	"go.temporal.io/sdk/workflow"
 	"time"
 )
@@ -12,6 +12,6 @@ func Workflow(ctx workflow.Context) (*s32.ListBucketsOutput, error) {
 		TaskQueue:           "aws-sdk",
 		StartToCloseTimeout: 10 * time.Second,
 	})
-	s3 := awsclients.NewS3Stub()
+	s3 := s3stub.NewClient()
 	return s3.ListBuckets(ctx, &s32.ListBucketsInput{})
 }
